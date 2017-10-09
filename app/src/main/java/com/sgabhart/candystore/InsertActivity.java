5fdccefad48bf25623d6a067b3cf94c3601f81d6
Spencer Gabhart <sgabhart2230@gmail.com>
2017-10-09 10:58:47 -0400

    Completed logic of db insertion

--- app/src/main/java/com/sgabhart/candystore/InsertActivity.java
+++ app/src/main/java/com/sgabhart/candystore/InsertActivity.java
@@ -5,14 +5,18 @@ package com.sgabhart.candystore;
  */
 
 import android.os.Bundle;
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InsertActivity extends AppCompatActivity {
+    private DatabaseManager dbManager;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        dbManager = new DatabaseManager(this);
         setContentView(R.layout.activity_insert);
     }
 
@@ -21,9 +25,17 @@ public class InsertActivity extends AppCompatActivity {
         EditText priceET = (EditText)(findViewById(R.id.input_price));
 
         String name = nameET.getText().toString();
-        String price = priceET.getText().toString();
+        String priceString = priceET.getText().toString();
 
-        // Insert candy data in db here
+        // Insert new candy data in db
+        try{
+            double price = Double.parseDouble(priceString);
+            Candy candy = new Candy(0, name, price);
+            dbManager.insert(candy);
+            Toast.makeText(this, "Candy added", Toast.LENGTH_SHORT).show();
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(this, "Price error", Toast.LENGTH_SHORT).show();
+        }
 
         nameET.setText("");
         priceET.setText("");
